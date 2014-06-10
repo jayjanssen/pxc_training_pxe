009adfe4e943bd7e7ce515a3a341c10a5bf0f8b2
Jay Janssen <jay.janssen@percona.com>
2014-06-10 12:42:11 -0400

    cron.sh remote fetch and execute script

---
+++ cron.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# This file gets pulled down OVER THE INTERNET when the classroom machines and re-run every 5 mins (new download every time)
+# The purpose here is to easily be able to customize the boxes and add newer files, etc. without refreshing the PXE boot server
+
+touch /tmp/it_works
