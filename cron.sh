96ca094d450bdd7179ac4fece28a4c8b40713ed3
Jay Janssen <jay.janssen@percona.com>
2014-06-10 13:24:39 -0400

    Remove scratch file

--- cron.sh
+++ cron.sh
@@ -3,8 +3,6 @@
 # This file gets pulled down OVER THE INTERNET when the classroom machines and re-run every 5 mins (new download every time)
 # The purpose here is to easily be able to customize the boxes and add newer files, etc. without refreshing the PXE boot server
 
-touch /tmp/it_works
-
 su -l student -c "mkdir /home/student/.sync"
 su -l student -c "mkdir /home/student/sync"
 
